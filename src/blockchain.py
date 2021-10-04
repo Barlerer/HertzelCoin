f7851c5f740516fc99a28e40c1080848dc5fad6e
Bar Lerer <barlerer@gmail.com>
2021-10-04 17:38:22 +0300

    Refactor to include block validator as an external class

--- src/blockchain.py
+++ src/blockchain.py
@@ -1,9 +1,11 @@
 from src.block import Block
+from src.blockvalidator import BlockValidator
 
 
 class Blockchain:
-    def __init__(self) -> None:
+    def __init__(self, validator: BlockValidator) -> None:
         self.blocks = []
+        self.validator = validator
         self._create_initial_block()
 
     def add_block(self, block: Block) -> Block:
@@ -11,10 +13,8 @@ class Blockchain:
             self.blocks.append(block)
             return block
         latest_block = self.get_latest_block()
-        if latest_block.block_id + 1 != block.block_id:
-            raise BlockIDMismatch(block.block_id, latest_block.block_id + 1)
-        if latest_block.hash() != block.prev_block_hash:
-            raise BlockHashMismatch()
+        # Will throw an expection if the wrong block is added
+        self.validator.validate_block(block, latest_block)
         self.blocks.append(block)
         return block
 
@@ -24,21 +24,3 @@ class Blockchain:
     def _create_initial_block(self):
         initial_block = Block(0, "initial hash", [])
         self.add_block(initial_block)
-
-
-class BlockIDMismatch(Exception):
-    def __init__(self, block_id, expected_id, *args: object) -> None:
-        self.block_id = block_id
-        self.expected_id = expected_id
-        super().__init__(*args)
-
-    def __str__(self) -> str:
-        return f"Non-consecutive block-id numbers! expected: {self.expected_id} but received: {self.block_id}"
-
-
-class BlockHashMismatch(Exception):
-    def __init__(self, *args: object) -> None:
-        super().__init__(*args)
-
-    def __str__(self) -> str:
-        return "Block hash does not match previous hash"
